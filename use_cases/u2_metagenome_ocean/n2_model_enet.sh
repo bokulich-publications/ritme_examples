29a73494ede18b9ae1635916ebd2a2e2188856ed
adamovanja <57316423+adamovanja@users.noreply.github.com>
2025-07-02 10:33:48 +0200

    remove R req and update config

--- use_cases/u2_metagenome_ocean/n2_model_enet.sh
+++ use_cases/u2_metagenome_ocean/n2_model_enet.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name="u2_enet_config"
 #SBATCH -A es_bokulich
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=50
+#SBATCH --cpus-per-task=20
 #SBATCH --time=119:59:59
-#SBATCH --mem-per-cpu=14336
+#SBATCH --mem-per-cpu=3072
 #SBATCH --output="%x_out.txt"
 #SBATCH --open-mode=append
 
