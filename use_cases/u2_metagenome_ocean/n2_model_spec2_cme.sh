0386f3c6bb6cec7302b63a7ec290b200b3bfa1be
adamovanja <57316423+adamovanja@users.noreply.github.com>
2025-08-26 21:06:10 +0200

    cme exp

---
+++ use_cases/u2_metagenome_ocean/n2_model_spec2_cme.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#SBATCH --job-name="u2_spec2_cme"
+#SBATCH -A es_bokulich
+#SBATCH --ntasks=100
+#SBATCH --cpus-per-task=2
+#SBATCH --time=119:59:59
+#SBATCH --mem-per-cpu=2560
+#SBATCH --output="/cluster/work/bokulich/adamova/ritme_usecase_runs/logs/%x_out.txt"
+#SBATCH --open-mode=append
+
+module load eth_proxy
+
+set -x
+
+echo "SLURM_CPUS_PER_TASK: $SLURM_CPUS_PER_TASK"
+echo "SLURM_GPUS: $SLURM_GPUS"
+
+# ! USER SETTINGS HERE
+# -> config file to use
+CONFIG="config/u2_spec2_cme.json"
+# -> path to the metadata file
+PATH_MD="../../data/u2_tara_ocean/md_tara_ocean.tsv"
+# -> path to the feature table file
+PATH_FT="../../data/u2_tara_ocean/otu_table_tara_ocean.tsv"
+# -> path to taxonomy file
+PATH_TAX="../../data/u2_tara_ocean/taxonomy_tara_ocean.qza"
+# -> path to phylogeny file
+PATH_PHYLO="../../data/u2_tara_ocean/fasttree_tree_rooted_proc_suna15.qza"
+# -> path to the .env file
+ENV_PATH="../../.env"
+# -> path to store model logs
+LOGS_DIR="/cluster/work/bokulich/adamova/ritme_usecase_runs"
+# -> path to data splits
+PATH_DATA_SPLITS="data_splits_u2"
+
+# if your number of threads are limited increase as needed
+ulimit -u 60000
+ulimit -n 524288
+# ! USER END __________
+
+# # Load environment variables from .env
+export $(grep -v '^#' "$ENV_PATH" | xargs)
+
+# # CLI version
+echo "Running split-train-test"
+ritme split-train-test $PATH_DATA_SPLITS $PATH_MD $PATH_FT --train-size 0.8 --seed 12
+
+echo "Running find-best-model-config"
+ritme find-best-model-config $CONFIG "${PATH_DATA_SPLITS}/train_val.pkl" --path-to-tax $PATH_TAX --path-to-tree-phylo $PATH_PHYLO --path-store-model-logs $LOGS_DIR
+
+echo "Running evaluate-tuned-models"
+# Read the value of "experiment_tag" from the config file
+experiment_tag=$(python -c "import json, sys; print(json.load(open('$CONFIG'))['experiment_tag'])")
+
+ritme evaluate-tuned-models "${LOGS_DIR}/${experiment_tag}" "${PATH_DATA_SPLITS}/train_val.pkl" "${PATH_DATA_SPLITS}/test.pkl"
+
+sstat -j $SLURM_JOB_ID
+
+# get elapsed time of job
+echo "TIME COUNTER:"
+sacct -j $SLURM_JOB_ID --format=elapsed --allocations
