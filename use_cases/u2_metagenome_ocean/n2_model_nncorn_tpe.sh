301e629477933e5316786b55cc57a89d4a2eee55
adamovanja <57316423+adamovanja@users.noreply.github.com>
2025-09-18 10:24:08 +0200

    resending to work folder

--- use_cases/u2_metagenome_ocean/n2_model_nncorn_tpe.sh
+++ use_cases/u2_metagenome_ocean/n2_model_nncorn_tpe.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=50
 #SBATCH --time=119:59:59
 #SBATCH --mem-per-cpu=9216
-#SBATCH --output="/cluster/project/bokulich/adamova/ritme_usecase_runs/logs/%x_out.txt"
+#SBATCH --output="/cluster/work/bokulich/adamova/ritme_usecase_runs/logs/%x_out.txt"
 #SBATCH --open-mode=append
 
 module load eth_proxy
@@ -30,7 +30,7 @@ PATH_PHYLO="../../data/u2_tara_ocean/fasttree_tree_rooted_proc_suna15.qza"
 # -> path to the .env file
 ENV_PATH="../../.env"
 # -> path to store model logs
-LOGS_DIR="/cluster/project/bokulich/adamova/ritme_usecase_runs"
+LOGS_DIR="/cluster/work/bokulich/adamova/ritme_usecase_runs"
 # -> path to data splits
 PATH_DATA_SPLITS="data_splits_u2"
 
--- use_cases/u3_mlp_prediction/n4_original_setup_galaxy_count_log.sh
+++ use_cases/u3_mlp_prediction/n4_original_setup_galaxy_count_log.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --time=23:59:59
 #SBATCH --mem-per-cpu=4096
-#SBATCH --output="/cluster/project/bokulich/adamova/ritme_usecase_runs/logs/%x_out.txt"
+#SBATCH --output="/cluster/work/bokulich/adamova/ritme_usecase_runs/logs/%x_out.txt"
 #SBATCH --open-mode=append
 
 module load eth_proxy
