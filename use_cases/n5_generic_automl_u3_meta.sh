47764c6d961045a7dc400492549d05b9ea88ef7f
adamovanja <57316423+adamovanja@users.noreply.github.com>
2025-10-08 20:05:35 +0200

    automl for u3

--- use_cases/n5_generic_automl_u3_meta.sh
+++ use_cases/n5_generic_automl_u3_meta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name="n5_automl_u3"
+#SBATCH --job-name="n5_automl_u3_meta"
 #SBATCH -A es_bokulich
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=50
@@ -12,11 +12,11 @@
 set -x
 
 python ../src/generic_automl.py \
-    --total-time-s 111576 \
-    --usecase u3 \
-    --data-splits-folder u3_mlp_prediction/data_splits_u3_galaxy_log \
-    --path-to-features ../data/u3_mlp_nishijima24/galaxy_otu_table.tsv \
-    --path-to-md ../data/u3_mlp_nishijima24/md_galaxy.tsv \
+    --total-time-s 115797 \
+    --usecase u3_meta \
+    --data-splits-folder u3_mlp_prediction/data_splits_u3_metacardis_log \
+    --path-to-features ../data/u3_mlp_nishijima24/metacardis_otu_table.tsv \
+    --path-to-md ../data/u3_mlp_nishijima24/md_metacardis.tsv \
     --target count_log10 \
     --single-best \
     --restricted-model gradient_boosting
