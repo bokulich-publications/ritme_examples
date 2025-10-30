2e5bcb608d7b6b22e3c1c838d38d952e790e263c
Anja Adamov <57316423+adamovanja@users.noreply.github.com>
2025-10-30 22:17:05 +0100

    newest usecase runs for euler

--- use_cases/u2_metagenome_ocean/submit_all.sh
+++ use_cases/u2_metagenome_ocean/submit_all.sh
@@ -6,14 +6,14 @@
 set -euo pipefail
 
 scripts=(
-  n2_model_linreg_tpe.sh
-  n2_model_linreg_tpe_restricted.sh
+  # n2_model_linreg_tpe.sh
+  # n2_model_linreg_tpe_restricted.sh
   n2_model_nnclass_tpe.sh
   n2_model_nncorn_tpe.sh
   n2_model_nnreg_tpe.sh
-  n2_model_rf_tpe.sh
-  n2_model_trac_tpe.sh
-  n2_model_xgb_tpe.sh
+  # n2_model_rf_tpe.sh
+  # n2_model_trac_tpe.sh
+  # n2_model_xgb_tpe.sh
 )
 
 for s in "${scripts[@]}"; do
--- use_cases/u3_mlp_prediction/n2_model_galaxy_log_nnreg_random.sh
+++ use_cases/u3_mlp_prediction/n2_model_galaxy_log_nnreg_random.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=100
 #SBATCH --mem-per-cpu=4096
 #SBATCH --time=119:59:59
-#SBATCH --output="/cluster/project/bokulich/adamova/ritme_usecase_runs_final_time/logs/%x_out.txt"
+#SBATCH --output="/scratch/adamova/ritme_usecase_runs_final_time/logs/%x_out.txt"
 #SBATCH --open-mode=append
 
 module load eth_proxy
@@ -28,7 +28,7 @@ PATH_TAX="../../data/u3_mlp_nishijima24/u3_taxonomy.qza"
 # -> path to the .env file
 ENV_PATH="../../.env"
 # -> path to store model logs
-LOGS_DIR="/cluster/project/bokulich/adamova/ritme_usecase_runs_final_time"
+LOGS_DIR="/scratch/adamova/ritme_usecase_runs_final_time"
 # -> path to data splits
 PATH_DATA_SPLITS="data_splits_u3_galaxy_log"
 
